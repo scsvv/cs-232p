51319ddfc29ca37d6e3711b255ac1eed6e44a529
Skorobohatov Sviatoslav <zvat.333333@gmail.com>
2023-07-10 17:26:30 +0300

    ls2

---
+++ README.md
@@ -0,0 +1,9 @@
+# CS Python 232 Group (Summer 2023)
+
+### Links 
+
+### Tasks 
+
+### Themes
+
+1. I/O | Algorithms;
---
+++ ls2/main.py
@@ -0,0 +1,14 @@
+# Оптимальный вес к росту  ОПТИМАЛЬНЫЙ ВЕС = РОСТ - 100 ()
+"""
+int() -> -1, 0, 1 
+float() -> 1.14
+str() -> "asd", 'asd', '1'
+bool() -> True, False
+
+type()
+"""
+height = input('Enter your height: ')
+print(f'Your optimal weight is: {int(height) - 100}')
+
+t, f = True, False
+print(t, f)
